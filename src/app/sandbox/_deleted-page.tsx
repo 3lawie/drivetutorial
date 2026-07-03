3ccb85e4760e8883e5cad1d6b9cca3beb093b193
3lawie <mly445032@gmail.com>
2026-07-03 12:36:52 +0300

    update

--- src/app/sandbox/_deleted-page.tsx
+++ src/app/sandbox/_deleted-page.tsx
@@ -9,30 +9,30 @@ import { FileKey } from "lucide-react";
 export default async function sandboxPage() {
     return <div className="flex flex-col gap-4">
         Seed Function
-        <form  action={async ()=>{
+        <form action={async () => {
             "use server"
             const user = await auth();
 
             if (!user.userId) throw new Error("Unauthorized");
-            
+
             const folders = await db.select().from(folders_table);
 
             const foldersIds = folders.map(folder => folder.id);
-            
+
             //const rootFolders = await db.insert(folders_table).values({
             //name:"root",
             //ownerId: user.userId,
             //parent:null,
             //}).$returningId();
-//
+            //
             //const insertableFolders = mockFolders.map((folder)=>({
             //    name: folder.name,
             //    ownerId: user.userId,
             //    parent: rootFolders[0]!.id,
             //}));
-//
+            //
             //await db.insert(folders_table).values(insertableFolders);
-            
+
             const insertableFiles = mockFiles.map((file) => ({
                 name: file.name,
                 ownerId: user.userId,
@@ -41,17 +41,17 @@ export default async function sandboxPage() {
                 fileKey: null,
                 url: file.url
             }));
-//
+            //
             await db.insert(files_table).values(insertableFiles);
-//
-        
-       
-    }
-}
-    >  
-       <button type="submit"> Seed </button>
-     
+            //
+
+
+        }
+        }
+        >
+            <button type="reset"> Seed </button>
+
         </form>
-     
+
     </div>
 }
